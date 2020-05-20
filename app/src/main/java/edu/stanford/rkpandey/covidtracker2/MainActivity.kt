cd92db70bb83e43eb6d06438f50c2906ff47671b
Rahul Pandey <hello@rkpandey.com>
2020-05-20 16:03:54 -0700

    add data class

--- app/src/main/java/edu/stanford/rkpandey/covidtracker2/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/covidtracker2/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
@@ -12,14 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val firestoreDb = FirebaseFirestore.getInstance()
+
         firestoreDb.collection("workers").addSnapshotListener { snapshot, exception ->
             if (exception != null || snapshot == null) {
                 Log.e(TAG, "Error retrieving worker info $exception")
                 return@addSnapshotListener
             }
+            var workersString = "WORKERS:\n"
             for (document in snapshot.documents) {
+                val worker = document.toObject(Worker::class.java)
                 Log.i(TAG, "Name: ${document.getString("name")}, role: ${document.getString("role")}")
+                Log.i(TAG, "$worker")
+                workersString += "${worker?.name}\n"
             }
+            tvWorkers.text = workersString
         }
     }
 }
---
+++ app/src/main/java/edu/stanford/rkpandey/covidtracker2/Worker.kt
@@ -0,0 +1,3 @@
+package edu.stanford.rkpandey.covidtracker2
+
+data class Worker(var name: String = "", var role: String = "", var level: Int = 0)

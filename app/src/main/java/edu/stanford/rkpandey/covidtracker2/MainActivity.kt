38c4cc60390ce32981e2d8b89f435c899756a4eb
Rahul Pandey <hello@rkpandey.com>
2020-05-20 15:42:21 -0700

    basic integration with firestore

--- app/src/main/java/edu/stanford/rkpandey/covidtracker2/MainActivity.kt
+++ app/src/main/java/edu/stanford/rkpandey/covidtracker2/MainActivity.kt
@@ -2,11 +2,24 @@ package edu.stanford.rkpandey.covidtracker2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val firestoreDb = FirebaseFirestore.getInstance()
+        firestoreDb.collection("workers").addSnapshotListener { snapshot, exception ->
+            if (exception != null || snapshot == null) {
+                Log.e(TAG, "Error retrieving worker info $exception")
+                return@addSnapshotListener
+            }
+            for (document in snapshot.documents) {
+                Log.i(TAG, "Name: ${document.getString("name")}, role: ${document.getString("role")}")
+            }
+        }
     }
 }
